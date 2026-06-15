d06cba3d5b540e65cceedde88f3ee9b09b7c546e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-15 23:06:41 +0100

    Update InstallMySQLClient.sh

--- installation/InstallMySQLClient.sh
+++ installation/InstallMySQLClient.sh
@@ -64,7 +64,7 @@ do
 			else
 				/usr/bin/wget https://dev.mysql.com/get/downloads/mysql-${major_version}/mysql-server_${minor_version}-1ubuntu${BUILDOS_VERSION}_amd64.deb-bundle.tar		
 				/usr/bin/tar -xvf ./mysql-server_${minor_version}-1ubuntu${BUILDOS_VERSION}_amd64.deb-bundle.tar -C /opt
-        		/bin/rm ./mysql-server_${minor_version}-1debian${BUILDOS_VERSION}_amd64.deb-bundle.tar
+        		/bin/rm ./mysql-server_${minor_version}-1ubuntu${BUILDOS_VERSION}_amd64.deb-bundle.tar
 	 			${install_command} libmecab2 libnuma1 psmisc 
 				DEBIAN_FRONTEND=noninteractive /usr/sbin/dpkg-preconfigure /opt/mysql-community-server_*.deb
 				/usr/bin/dpkg -i /opt/mysql-common_*.deb

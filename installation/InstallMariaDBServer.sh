386e355b88fc9cd51455bb96209515285ba1bea0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 14:17:55 +0100

    Update InstallMariaDBServer.sh

--- installation/InstallMariaDBServer.sh
+++ installation/InstallMariaDBServer.sh
@@ -37,6 +37,8 @@ BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 BUILDOS_VERSION="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOSVERSION'`"
 
 manager=""
+options=""
+tail_options=""
 if ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
 then
 	manager="/usr/bin/apt"

2c5ee4670c5fa9ecc9e2ee17128bdaf591641089
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 14:36:47 +0100

    Update RemoveUnattendedUpgrades.sh

--- installation/RemoveUnattendedUpgrades.sh
+++ installation/RemoveUnattendedUpgrades.sh
@@ -42,6 +42,10 @@ elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" 
 then
 	manager="/usr/bin/apt-get"
 	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
+elif ( [ "`${HOME}/utilities/config/ExtractBuildStyleValues.sh "PACKAGEMANAGER" | /usr/bin/awk -F':' '{print $NF}'`" = "nala" ] )
+then
+	manager="/usr/bin/nala"
+	tail_options="-y"
 fi
 
 export DEBIAN_FRONTEND=noninteractive
@@ -51,11 +55,11 @@ if ( [ "${manager}" != "" ] )
 then
 	if ( [ "${BUILDOS}" = "ubuntu" ] )
 	then
-		eval ${remove_command} unattended-upgrades	
+		eval ${remove_command} unattended-upgrades	${tail_options}
 	fi
 
 	if ( [ "${BUILDOS}" = "debian" ] )
 	then
-		eval ${remove_command} unattended-upgrades	
+		eval ${remove_command} unattended-upgrades ${tail_options}
 	fi
 fi

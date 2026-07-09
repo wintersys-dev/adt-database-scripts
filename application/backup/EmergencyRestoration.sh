6bd0649b97109f78a0f57893ca98f110b4e42de8
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-09 11:09:37 +0100

    Modify database name format in restoration script

--- application/backup/EmergencyRestoration.sh
+++ application/backup/EmergencyRestoration.sh
@@ -16,7 +16,8 @@ ${HOME}/services/database/BackupDatabase.sh "${HOME}/runtime/restoration_archive
 
 ${HOME}/services/database/InitialiseDatabase.sh 
 DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
-/bin/grep -rlZ ${DB_N} ${HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N}/${DB_N}.${archive_id}/g"
+DB_N1="`/bin/echo .${archive_id} | /bin/sed -e 's/\./_/g' -e 's/-/_/g'`"
+/bin/grep -rlZ ${DB_N} ${HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N}/${DB_N1}/g"
 ${HOME}/application/db/InstallApplicationDB.sh "${archive_id}"
 
 /bin/rm ${HOME}/runtime/restoration_archives/ARCHIVE_ID

7be533b9bce712b45c0284af2244459a1bf727b0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-11 10:20:04 +0100

    Update EmergencyRestoration.sh for archive ID and DBNAME
    Normalize archive ID to lowercase and fix DBNAME extraction pattern.

--- application/backup/EmergencyRestoration.sh
+++ application/backup/EmergencyRestoration.sh
@@ -3,6 +3,7 @@
 
 HOME="`/bin/cat /home/homedir.dat`"
 archive_id="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "ACTIVATE_RESTORATION.ARCHIVE" | /bin/sed 's/ACTIVATE_RESTORATION\.//g'`"
+archive_id="`/bin/echo ${archive_id} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 
 if ( [ ! -d ${HOME}/runtime/restoration_archives ] )
 then
@@ -15,7 +16,7 @@ fi
 /bin/echo ${archive_id} > ${HOME}/runtime/restoration_archives/ARCHIVE_ID
 
 ${HOME}/services/database/InitialiseDatabase.sh
-DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME' | /bin/sed 's/_ARCHIVE.*//g'`"
+DB_N="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME' | /bin/sed 's/_archive.*//g'`"
 DB_N1="`/bin/echo .${archive_id} | /bin/sed -e 's/\./_/g' -e 's/-/_/g'`"
 DB_N2="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBNAME'`"
 /bin/grep -rlZ ${DB_N2} ${HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_N2}/${DB_N}${DB_N1}/g"

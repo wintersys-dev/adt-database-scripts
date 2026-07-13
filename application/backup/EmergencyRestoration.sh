e7b1446f3481c8d70821df9e45523d048566c1a8
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-13 22:44:11 +0100

    Update EmergencyRestoration.sh

--- application/backup/EmergencyRestoration.sh
+++ application/backup/EmergencyRestoration.sh
@@ -5,6 +5,11 @@ HOME="`/bin/cat /home/homedir.dat`"
 archive_id="`${HOME}/services/datastore/config/wrapper/ListFromDatastore.sh "config" "ACTIVATE_RESTORATION.ARCHIVE" | /bin/sed 's/ACTIVATE_RESTORATION\.//g'`"
 archive_id="`/bin/echo ${archive_id} | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 
+if ( [ "${archive_id}" = "" ] )
+then
+        exit
+fi
+
 if ( [ ! -d ${HOME}/runtime/restoration_archives ] )
 then
         /bin/mkdir -p ${HOME}/runtime/restoration_archives

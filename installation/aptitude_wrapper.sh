d96affba5026bf53ad43517c8f1cd4a0cc7b2b81
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-16 19:19:30 +0100

    Update aptitude_wrapper.sh

--- installation/aptitude_wrapper.sh
+++ installation/aptitude_wrapper.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if ( [ ! -f  /var/lib/aptitude/pkgstates ] )
+then
+        /bin/touch  /var/lib/aptitude/pkgstates
+fi
+
 while ( [ "`/usr/bin/fuser /var/lib/dpkg/lock-frontend`" != "" ] )
 do
         echo "Waiting for another package manager process to complete..."

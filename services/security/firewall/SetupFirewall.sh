d178f19e09d2b8d51ce95c2c25771d82166d1f3e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 08:41:27 +0100

    Redirect SSH config updates to hardening.conf

--- services/security/firewall/SetupFirewall.sh
+++ services/security/firewall/SetupFirewall.sh
@@ -129,7 +129,7 @@ if ( [ "`/bin/grep ${VPC_IP_RANGE} /etc/ssh/sshd_config`" = "" ] )
 then
 	if ( [ "${SERVER_USER}" != "" ] && [ "${VPC_IP_RANGE}" != "" ] )
 	then
-		/bin/echo "AllowUsers ${SERVER_USER}@${VPC_IP_RANGE}" >> /etc/ssh/sshd_config
+		/bin/echo "AllowUsers ${SERVER_USER}@${VPC_IP_RANGE}" >> /etc/ssh/sshd_config.d/99-hardening.conf
 		updated_ssh="1"
 	fi
 fi
@@ -138,7 +138,7 @@ if ( [ "`/bin/grep ${BUILD_MACHINE_IP} /etc/ssh/sshd_config`" = "" ] )
 then
 	if ( [ "${SERVER_USER}" != "" ] && [ "${BUILD_MACHINE_IP}" != "" ] )
 	then
-		/bin/echo "AllowUsers ${SERVER_USER}@${BUILD_MACHINE_IP}" >> /etc/ssh/sshd_config
+		/bin/echo "AllowUsers ${SERVER_USER}@${BUILD_MACHINE_IP}" >> /etc/ssh/sshd_config.d/99-hardening.conf
 		updated_ssh="1"
 	fi
 fi

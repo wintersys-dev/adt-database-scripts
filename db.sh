f1311010bed2a5d8408b8276f5f5c6756756cbe2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 08:17:09 +0100

    Fix logging and variable extraction in db.sh

--- db.sh
+++ db.sh
@@ -114,6 +114,8 @@ exec 1>>${HOME}/logs/${out_file}
 err_file="initialbuild/database-build-err-`/bin/date | /bin/sed 's/ //g'`"
 exec 2>>${HOME}/logs/${err_file}
 
+/usr/sbin/sshd -T >  ${HOME}/logs/initialbuild/sshd-initial-settings.log
+
 BUILD_ARCHIVE_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDARCHIVECHOICE'`"
 GIT_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'GITUSER' | /bin/sed 's/#/ /g'`"
 GIT_EMAIL_ADDRESS="`${HOME}/utilities/config/ExtractConfigValue.sh 'GITEMAILADDRESS'`"

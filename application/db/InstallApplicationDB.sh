dafa13572d698b205eaa9883f6868557551754f9
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 14:16:09 +0100

    Change email subject for installation error notification

--- application/db/InstallApplicationDB.sh
+++ application/db/InstallApplicationDB.sh
@@ -163,6 +163,6 @@ fi
 # We reckon all is good if this file exists
 if ( [ ! -f ${HOME}/runtime/DB_APPLICATION_INSTALLED ] )
 then
-        ${HOME}/services/email/SendEmail.sh "DEFINITE INSTALLATION ERROR" "The application database  didn't import correctly into the database system" "ERROR"
+        ${HOME}/services/email/SendEmail.sh "DATABASE IMPORT ERROR" "The application database  didn't import correctly into the database system" "ERROR"
 fi
 

35ea9a63f8c91201960390893e99b0201027da86
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 08:56:32 +0100

    Update ArchiveErrorLogs.sh

--- utilities/housekeeping/ArchiveErrorLogs.sh
+++ utilities/housekeeping/ArchiveErrorLogs.sh
@@ -1,1 +1,27 @@
+#set -x
 
+current_day="`/usr/bin/date +%d`"
+current_month="`/usr/bin/date +%m | /bin/sed 's/^0//g'`"
+current_year="`/usr/bin/date +%y`"
+archive_day="`/usr/bin/expr ${current_day} - 1`"
+archive_date="archive-${archive_day}-${current_month}-${current_year}"
+
+if ( [ ! -d ${HOME}/logs/archives/${archive_date} ] )
+then
+        /bin/mkdir -p ${HOME}/logs/archives/${archive_date}
+fi
+
+cd ${HOME}/logs
+/bin/tar cvfz ${HOME}/logs/archives/${archive_date}/archive.tar.gz --exclude "./archives" .
+
+if ( [ ! -d ${HOME}/runtime/logging_archive_workarea ] )
+then
+        /bin/mkdir -p ${HOME}/runtime/logging_archive_workarea
+fi
+
+if ( [ "$?" = "0" ] )
+then
+        /bin/mv ${HOME}/logs/archives ${HOME}/runtime/logging_archive_workarea
+        /bin/rm -r ${HOME}/logs/*
+        /bin/mv ${HOME}/runtime/logging_archive_workarea/archives ${HOME}/logs/
+fi

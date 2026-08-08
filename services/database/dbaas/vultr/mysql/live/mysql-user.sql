4ce857494b7dabccbd8056de9d5347d442ea7b22
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-08 09:43:06 +0100

    Require SSL for MySQL user creation

--- services/database/dbaas/vultr/mysql/live/mysql-user.sql
+++ services/database/dbaas/vultr/mysql/live/mysql-user.sql
@@ -1,4 +1,4 @@
 use mysql;
 SET sql_require_primary_key=0;
-CREATE USER 'XXXXDB_UXXXX'@'%' IDENTIFIED BY 'XXXXDB_PXXXX';
+CREATE USER 'XXXXDB_UXXXX'@'%' IDENTIFIED BY 'XXXXDB_PXXXX' REQUIRE SSL;
 flush privileges;

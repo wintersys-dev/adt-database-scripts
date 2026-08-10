64542851de47ffeb0901e2950970f0a6b2ec70b2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-11 00:27:20 +0100

    Update IsDatabaseUp.sh

--- utilities/status/IsDatabaseUp.sh
+++ utilities/status/IsDatabaseUp.sh
@@ -1,26 +1,4 @@
 #!/bin/sh
-#####################################################################################
-# Author : Peter Winter
-# Date   : 10/4/2016
-# Description : Check if the database is up and running
-#####################################################################################
-# License Agreement:
-# This file is part of The Agile Deployment Toolkit.
-# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################################
-#######################################################################################################
-#set -x
-
-#!/bin/sh
 #################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
@@ -41,6 +19,11 @@
 #####################################################################################
 #####################################################################################
 set -x
+
+if ( [ ! -f ${HOME}/runtime/INSTALLED_SUCCESSFULLY ] )
+then
+	exit
+fi
 
 running="0"
 ${HOME}/utilities/remote/ConnectToRemoteMySQL.sh "exit"

f54618fd17b4b4e0795352e312320dde3144887c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-23 11:14:44 +0100

    Implement retry mechanism in aptitude_wrapper.sh
    Add retry logic for aptitude command execution.

--- installation/aptitude_wrapper.sh
+++ installation/aptitude_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/bin/touch  /var/lib/aptitude/pkgstates
+set -x
 
 while ( [ "`/usr/bin/fuser /var/lib/dpkg/lock-frontend`" != "" ] )
 do
@@ -10,4 +10,23 @@ done
 
 /usr/bin/dpkg --configure -a
 command="/usr/bin/aptitude $@"
-eval "${command}"
+
+success="0"
+count="0"
+
+while ( [ "${success}" = "0" ] && [ "${count}" -lt "30" ] )
+do
+        eval "${command}"
+        if ( [ "$?" = "0" ] )
+        then    
+                success="1"
+        else
+                count="`/usr/bin/expr ${count} + 1`"
+                /bin/sleep 5
+        fi
+done
+
+if ( [ "${count}" = "30" ] )
+then
+        /bin/echo "command: ${command} failed"
+fi
